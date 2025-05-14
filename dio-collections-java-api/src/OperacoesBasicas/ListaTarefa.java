ea4a271d998d5b045464352d09d5d0711923d8e5
Thiago Renan <thiagorenan1108@gmail.com>
2025-05-14 15:57:30 -0300

    Lista de Tarefas

---
+++ dio-collections-java-api/src/OperacoesBasicas/ListaTarefa.java
@@ -0,0 +1,75 @@
+package OperacoesBasicas;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ListaTarefa {
+    //Atributo
+    private List<Tarefa> tarefaList;
+
+
+    public ListaTarefa() {
+        this.tarefaList = new ArrayList<>();
+    }
+
+    public ListaTarefa(List<Tarefa> tarefaList) {
+        this.tarefaList = tarefaList;
+
+    }
+
+    public void adicionarTarefa(String descricao) {
+        tarefaList.add(new Tarefa(descricao));
+
+    }
+
+    public void removerTarefa(String descricao) {
+       List<Tarefa> tarefasParaRemover = new ArrayList<>();
+        for(Tarefa t: tarefaList ){
+            if(t.getDescricao().equalsIgnoreCase(descricao)){
+                tarefasParaRemover.add(t);
+            }
+        }
+        tarefaList.removeAll(tarefasParaRemover);
+
+    }
+
+    public int obterNumeroTotalTarefa() {
+        return tarefaList.size();
+    }
+
+    public void obterDescricoesTarefa()  {
+        System.out.println(tarefaList);
+    }
+
+    public List<Tarefa> getTarefaList() {
+        return tarefaList;
+    }
+
+    public void setTarefaList(List<Tarefa> tarefaList) {
+        this.tarefaList = tarefaList;
+    }
+
+
+    public static void main (String[] args){
+
+        ListaTarefa listaTarefa = new ListaTarefa();
+
+        System.out.println("O número total de elementos na lista é:" + listaTarefa.obterNumeroTotalTarefa());
+
+        listaTarefa.adicionarTarefa("Tarefa 01");
+        listaTarefa.adicionarTarefa("Tarefa 02");
+        listaTarefa.adicionarTarefa("Tarefa 03");
+        System.out.println("O número total de elementos na lista é:" + listaTarefa.obterNumeroTotalTarefa());
+
+        listaTarefa.removerTarefa("Tarefa 01");
+        System.out.println("O número total de elementos na lista é:" + listaTarefa.obterNumeroTotalTarefa());
+
+        listaTarefa.obterDescricoesTarefa();
+
+
+    }
+}
+
+
+
+
