555d5ad0c75e83c35ab48cc92a3e6a300fcc5155
Thiago Renan <thiagorenan1108@gmail.com>
2025-05-17 19:09:03 -0300

    Cadastro Produtos - Comparable<T> / Comparator<T>

---
+++ dio-collections-java-api/src/Ordenacao/Produto.java
@@ -0,0 +1,77 @@
+package Ordenacao;
+
+import java.util.Objects;
+
+public class Produto implements Comparable<Produto>{
+
+    private long codigo;
+    private String nome;
+    private double preco;
+    private int quantidade;
+
+    public Produto(long codigo , String nome, double preco, int quantidade) {
+        this.codigo  = codigo ;
+        this.nome = nome;
+        this.preco = preco;
+        this.quantidade = quantidade;
+    }
+
+    @Override
+    public int compareTo(Produto p) {
+        return nome.compareToIgnoreCase(p.getNome());
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public long getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(int codigo) {
+        this.codigo  = codigo;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(double preco) {
+        this.preco = preco;
+    }
+
+    public int getQuantidade() {
+        return quantidade;
+    }
+
+    public void setQuantidade(int quantidade) {
+        this.quantidade = quantidade;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Produto produto = (Produto) o;
+        return getCodigo() == produto.getCodigo();
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(getCodigo());
+    }
+
+    @Override
+    public String toString() {
+        return "Produto{" +
+                 nome + '\'' +
+                ", cod = " +  codigo  +
+                ", preco = R$" + preco +
+                ", quantidade = " + quantidade +
+                "} \n";
+    }
+}
